24225068b73c49770fc982780c3b059ac2c552ad
mim-mel <mfblade2014@gmail.com>
2022-10-21 17:58:46 +0900

    feat: mockdata 파일 생성

---
+++ src/mockdata.jsx
@@ -0,0 +1,110 @@
+export const auditionMockData = [
+  {
+    theme: '웹드라마',
+    date: 'D-11',
+    title: '[배우 모집]플필X틱톡 2022웹드라마 오디션',
+    subTitle: '플필X틱톡',
+    endDate: '2022-10-31 마감',
+    src: 'image/poster3.jpg',
+    color: 'rgb(246, 244, 182)',
+  },
+  {
+    theme: '웹드라마',
+    date: 'D-2',
+    title:
+      '미래에셋 버추얼 웹드라마 [우리의 미래(가제)] 에서 여주인공을 찾습니다 (내용 확인 필수)',
+    subTitle: '미래에셋',
+    endDate: '2022-10-22 마감',
+    src: 'image/poster4.jpg',
+    color: 'rgb(246, 244, 182)',
+  },
+  {
+    theme: '기타',
+    date: 'D-26',
+    title: '국.내.최.초. 윤봉길 의사 오디션 *대전mbc방영',
+    subTitle: '대전MBC',
+    endDate: '2022-11-15 마감',
+    src: 'image/poster1.jpg',
+    color: 'rgb(231, 241, 246)',
+  },
+  {
+    theme: 'TV드라마',
+    date: 'D-1',
+    title: 'OTT 드라마 고등학교 테니스 부원 모집 (내용 확인 필수)',
+    subTitle: '필름케이',
+    endDate: '2022-10-21 마감',
+    src: 'image/poster2.jpg',
+    color: 'rgb(187, 248, 186)',
+  },
+];
+
+export const actorMockData = [
+  {
+    src: 'image/actor1.jpg',
+    name: '이동우',
+    age: '33세 배우',
+  },
+  {
+    src: 'image/actor2.jpg',
+    name: '하유림',
+    age: '22세 배우',
+  },
+  {
+    src: 'image/actor3.jpg',
+    name: '한성우',
+    age: '25세 배우',
+  },
+  {
+    src: 'image/actor4.jpg',
+    name: '한홍규',
+    age: '29세 배우',
+  },
+  {
+    src: 'image/actor5.jpg',
+    name: '주여름',
+    age: '25세 배우',
+  },
+  {
+    src: 'image/actor6.jpg',
+    name: '최민기',
+    age: '24세 배우',
+  },
+  {
+    src: 'image/actor7.jpg',
+    name: '장민혁',
+    age: '24세 배우',
+  },
+  {
+    src: 'image/actor8.jpg',
+    name: '손혜리',
+    age: '24세 배우',
+  },
+  {
+    src: 'image/actor9.jpg',
+    name: '유지완',
+    age: '6세 배우',
+  },
+  {
+    src: 'image/actor10.jpg',
+    name: '여소영',
+    age: '22세 배우',
+  },
+];
+
+export const communityMockdata = [
+  {
+    title: '필톡 제시대사는 어디있나요?',
+    writer: '권기하',
+    date: '10-19',
+  },
+  {
+    title: '<10.19헬스장 2시간> 갬성샷2',
+    writer: '고마운 곰',
+    date: '10-19',
+  },
+  {
+    title: '2022 플필X틱톡 웹드라마 오디션',
+    writer: 'plfilcm',
+    date: '10-19',
+  },
+];
